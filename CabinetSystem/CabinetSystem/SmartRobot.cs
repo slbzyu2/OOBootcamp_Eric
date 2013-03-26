7531276fb390086f95f06d41d00a86ba16132c41
Eric <ericy@live.cn>
2013-03-26 13:08:04 +0800

    create  test case: test_smartrobot_should_has_empty_box & test_smartrobot_can_store_bag

---
+++ CabinetSystem/CabinetSystem/SmartRobot.cs
@@ -0,0 +1,23 @@
+using CabinetSystemTest;
+
+namespace CabinetSystem
+{
+    public class SmartRobot
+
+    {
+        public bool HasEmptyBox()
+        {
+            return true;
+        }
+
+        public void Add(Cabinet cabinet)
+        {
+            return;
+        }
+
+        public Ticket Store(Bag bag)
+        {
+            return new Ticket();
+        }
+    }
+}
